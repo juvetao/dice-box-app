352582075356f356f0593ec8c5d9c66f4030e3e0
Cheng <cheng.tao@hiq.se>
2020-09-25 16:23:48 +0200

    Add special background color to show the dice is locked or not

--- src/dice.js
+++ src/dice.js
@@ -2,10 +2,17 @@ import React from "react"
 import "./dice.css"
 
 export default function Dice(props){
+    //add different class in css to show the dice locked or not
+    if(props.dice.locked){
     return(
         //add the property of click in Dice into div's onclick event
-        <div className="dice" onClick={()=>props.click()}>{props.dice.value}</div>
-    )
+        <div className="dice locked" onClick={()=>props.click()}>{props.dice.value}</div>
+    );
+    }else{
+        return(
+            <div className="dice" onClick={()=>props.click()}>{props.dice.value}</div>
+        );
+    }
 }
     
 
